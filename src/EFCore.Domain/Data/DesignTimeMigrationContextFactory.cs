1878824f4593f7e73916912890dce159487f92d5
Chris Klug <chris@59north.com>
2023-10-09 17:35:26 +0200

    Added designtime migration factory

---
+++ src/EFCore.Domain/Data/DesignTimeMigrationContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EFCore.Domain.Data;
+
+public class DesignTimeMigrationContextFactory : IDesignTimeDbContextFactory<DemoContext>
+{
+    public DemoContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<DemoContext>();
+        builder.UseSqlServer();
+        return new DemoContext(builder.Options);
+    }
+}
