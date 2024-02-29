43ed9ed231d4d9e8e092c35165671f59ed998cfa
Chris Klug <chris@59north.com>
2024-02-29 07:52:02 +0100

    Added validation to Name

--- src/EFCore.DTO.Wrapper/Entities/Name.cs
+++ src/EFCore.DTO.Wrapper/Entities/Name.cs
@@ -1,3 +1,19 @@
 ﻿namespace EFCore.DTO.Wrapper.Entities;
 
-public record Name(string FirstName, string LastName);
+public record Name
+{
+    public Name(string firstName, string lastName)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new ArgumentNullException(nameof(firstName));
+        
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new ArgumentNullException(nameof(lastName));
+        
+        FirstName = firstName;
+        LastName = lastName;
+    }
+    
+    public string FirstName { get; }
+    public string LastName { get; }
+}
